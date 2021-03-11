99226fefc569ea6ecf3b2ec8b3a30274b2bce536
mustafasameturan <mustafasameturan@hotmail.com>
2021-03-11 21:30:56 +0300

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'rentACar';
+  title: string = 'RentACar';
+  user: string = 'Mustafa Turan';
+
+  car1: any = { carId: 1, Model: 'Clio', brandId: 1, dailyPrice: 100 };
+  car2: any = { carId: 2, Model: 'Megane 3', brandId: 1, dailyPrice: 150 };
+  car3: any = { carId: 3, Model: 'AMG', brandId: 2, dailyPrice: 400 };
+  car4: any = { carId: 4, Model: 'Focus', brandId: 3, dailyPrice: 100 };
+
+  cars = [this.car1, this.car2, this.car3, this.car4];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    NaviComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
